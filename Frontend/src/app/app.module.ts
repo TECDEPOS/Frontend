153c37dc12acf0e3e4f5c422bcb94b63d239693f
Jeppe <jfrimannlarsen@gmail.com>
2023-08-24 13:29:36 +0200

    NavBar + AuthGuard

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/loginPage/login/login.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/Misc/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/components/loginPage/login/login.component.ts
+++ Frontend/src/app/components/loginPage/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class LoginComponent {
 
   invalidLogin: boolean = false;
-
+  
   submitLogin(): void{
     
   }
