9c38aa31f6f2b6284548a709bb9e4ea803bd4c08
Snixert <swattlebagger@gmail.com>
2023-08-24 08:08:55 +0200

    changed names

--- Frontend/src/app/Models/ViewModels/AuthenticatedResponse.ts
+++ Frontend/src/app/Models/ViewModels/AuthenticatedResponse.ts
@@ -1,4 +1,4 @@
-export class AutehnticatedResponse{
+export class AuthenticatedResponse{
     userId: number = 0;
     userName: string = "";
     accessToken: string = "";
--- Frontend/src/app/Services/auth.service.ts
+++ Frontend/src/app/Services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { LoginViewModel } from '../Models/ViewModels/LoginViewModel';
-import { AutehnticatedResponse } from '../Models/ViewModels/AuthenticatedResponse';
+import { AuthenticatedResponse } from '../Models/ViewModels/AuthenticatedResponse';
 import { changePasswordViewModel } from '../Models/ViewModels/ChangePasswordViewModel';
 
 @Injectable({
@@ -16,15 +16,15 @@ export class AuthService {
   baseApiUrl: string = environment.baseApiUrl;
   extendedApiUrl: string = "Auth/"
 
-  addLogin(loginViewModel: LoginViewModel): Observable<AutehnticatedResponse>{
-    return this.http.post<AutehnticatedResponse>(this.baseApiUrl + this.extendedApiUrl + 'login', loginViewModel)
+  login(loginViewModel: LoginViewModel): Observable<AuthenticatedResponse>{
+    return this.http.post<AuthenticatedResponse>(this.baseApiUrl + this.extendedApiUrl + 'login', loginViewModel)
   }
 
   changePassword(viewModel: changePasswordViewModel): Observable<boolean>{
     return this.http.put<boolean>(this.baseApiUrl +this.extendedApiUrl + 'changepassword', viewModel)
   }
 
-  addRefreshToken(authResponse: AutehnticatedResponse): Observable<AutehnticatedResponse>{
-    return this.http.post<AutehnticatedResponse>(this.baseApiUrl + this.extendedApiUrl + 'refresh', authResponse)
+  addRefreshToken(authResponse: AuthenticatedResponse): Observable<AuthenticatedResponse>{
+    return this.http.post<AuthenticatedResponse>(this.baseApiUrl + this.extendedApiUrl + 'refresh', authResponse)
   }
 }
