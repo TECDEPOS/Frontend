c3e8df4ab1f45ddaebbc683e12760b55cff87720
Jeppe <88774896+HildaTipsyToe@users.noreply.github.com>
2024-02-15 10:53:59 +0100

    popup snackbar til ændring af slutdato automatisk skal updateres

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { MomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/loginPage/login/login.component';
@@ -76,6 +77,7 @@ export const MY_DATE_FORMATS = {
     AddPersonCourseComponent,
     EditPersonmodulePopupComponent,
     ModuleComponent,
+
     PasswordExpiredPopupComponent,
     CoursePageComponent,
     CoursesComponent
@@ -86,6 +88,7 @@ export const MY_DATE_FORMATS = {
     AppRoutingModule,
     MatInputModule,
     MatFormFieldModule,
+    MatSnackBarModule,
     MatIconModule,
     HttpClientModule,
     MatSelectModule,
