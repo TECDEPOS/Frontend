98c4737d5f56111c0868a23310ad72f87691bc9b
Snixert <jwn@PROELE10>
2024-03-12 07:59:35 +0100

    trying something new

--- Frontend/src/app/components/Misc/multi-dropdown/multi-dropdown.component.ts
+++ Frontend/src/app/components/Misc/multi-dropdown/multi-dropdown.component.ts
@@ -13,36 +13,49 @@ export class MultiDropdownComponent {
   everything: any = 'Alle'
   selectionValues: any[] = [];
   previousSelection: any[] = [];
-  ngOnInit(){
+  searchTerm: string = '';
+  filteredValues: any[] = [];
+
+  ngOnChanges(){    
     this.selectionValues = JSON.parse(JSON.stringify(this.values));
     this.previousSelection = JSON.parse(JSON.stringify(this.values));
+    // this.onSearchChanged();
   }
 
-  resetSelectionValues(){
-    this.selectionValues = [];
-    this.previousSelection = [];
-  }
+  // resetSelectionValues(){
+  //   this.selectionValues = [];
+  //   this.previousSelection = [];
+  // }
 
-  onSelectionChanged(): void {
-    const isEverythingSelected = this.selectionValues.includes(this.everything);
-    const wasEverythingSelected = this.previousSelection.includes(this.everything);
+  // onSelectionChanged(): void {
+  //   const isEverythingSelected = this.selectionValues.includes(this.everything);
+  //   const wasEverythingSelected = this.previousSelection.includes(this.everything);
 
-    // Check if 'Everything' was the last selected value
-    if (isEverythingSelected && !wasEverythingSelected) {
-      // Remove everything except 'Alle' if 'Alle' is selected and it wasn't before
-      this.selectionValues = this.selectionValues.filter(value => value === this.everything);
-      this.selectionChanged.emit([]);
-    }
-    else if (wasEverythingSelected && this.selectionValues.length > 1) {
-      // Remove 'Alle' selection if it's selected and something else is selected
-      this.selectionValues = this.selectionValues.filter(value => value !== this.everything);
-      this.selectionChanged.emit(this.selectionValues);
-    }
-    else{
-      this.selectionChanged.emit(this.selectionValues);
-    }
+  //   // Check if 'Everything' was the last selected value
+  //   if (isEverythingSelected && !wasEverythingSelected) {
+  //     // Remove everything except 'Alle' if 'Alle' is selected and it wasn't before
+  //     this.selectionValues = this.selectionValues.filter(value => value === this.everything);
+  //     this.selectionChanged.emit([]);
+  //   }
+  //   else if (wasEverythingSelected && this.selectionValues.length > 1) {
+  //     // Remove 'Alle' selection if it's selected and something else is selected
+  //     this.selectionValues = this.selectionValues.filter(value => value !== this.everything);
+  //     this.selectionChanged.emit(this.selectionValues);
+  //   }
+  //   else{
+  //     this.selectionChanged.emit(this.selectionValues);
+  //   }
 
-    // Update the previous selection state
-    this.previousSelection = this.selectionValues;
-  }
+  //   // Update the previous selection state
+  //   this.previousSelection = this.selectionValues;
+  // }
+
+  // onSearchChanged(){
+  //   if (this.searchTerm === '') {
+  //     this.filteredValues = this.values;
+  //   }
+  //   else{
+  //     this.filteredValues = this.values.filter(item => item.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
+  //   }
+  // }
 }
