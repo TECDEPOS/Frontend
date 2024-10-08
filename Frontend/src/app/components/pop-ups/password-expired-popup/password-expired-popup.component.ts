a800c2a8895a1669571a5baab67fa3f4c4b7180f
Sebastian Munch <seba2068@elev.tec.dk>
2024-10-08 13:57:33 +0200

    more comments

--- Frontend/src/app/components/pop-ups/password-expired-popup/password-expired-popup.component.ts
+++ Frontend/src/app/components/pop-ups/password-expired-popup/password-expired-popup.component.ts
@@ -9,7 +9,6 @@ import { AuthService } from 'src/app/Services/auth.service';
   styleUrls: ['./password-expired-popup.component.css']
 })
 export class PasswordExpiredPopupComponent {
-
   id:number = 999
   changePasswordModel: changePasswordViewModel = new changePasswordViewModel;
   constructor(private dialogRef: MatDialogRef<PasswordExpiredPopupComponent>, private authService: AuthService) {}
@@ -19,17 +18,21 @@ export class PasswordExpiredPopupComponent {
     this.id = this.authService.getUserId();
   }
 
-  closeDialog() {
-    this.dialogRef.close();
-  }
+  // Close the dialog when called
+closeDialog() {
+  this.dialogRef.close();
+}
 
-  changepassword(){
-    console.log(this.id);
-    console.log(this.changePasswordModel);
-    
-    this.authService.changePassword(this.changePasswordModel).subscribe(value =>{
-      console.log(value);
-      this.dialogRef.close();
-    })
-  }
+// Change the user's password using the provided model
+changepassword() {
+  console.log(this.id); // Log the user ID
+  console.log(this.changePasswordModel); // Log the change password model
+  
+  // Call the authentication service to change the password
+  this.authService.changePassword(this.changePasswordModel).subscribe(value => {
+      console.log(value); // Log the response from the service
+      this.dialogRef.close(); // Close the dialog after changing the password
+  });
+}
+
 }
